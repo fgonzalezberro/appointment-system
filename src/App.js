4da1ff84279b2f1f21206bd46556bb4f67b0b84c
fgonzalezberro <fgonzalezberro@gmail.com>
2021-03-09 22:37:36 -0300

    Create form component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
+// Import React 
+import React, {Fragment} from 'react';
+
+// Import components
+import Formulario from './components/Formulario';
+
+// Import CSS 
 import './index.css';
+
 
 function App() {
   return (
-    <div className="App">
+    <Fragment>
       <h1>Administrador de citas</h1>
-    </div>
+      <div className='container'>
+        <div className='row'>
+          <div className='one-half column'>
+            <Formulario />
+          </div>
+
+          <div className='one-half column'>
+            2
+          </div>
+        </div>
+      </div>
+    </Fragment>
+
   );
 }
 
---
+++ src/components/Formulario.js
@@ -0,0 +1,11 @@
+// Import react
+import React from 'react';
+
+// Formulario component
+const Formulario = () =>{
+    return(
+      <h1>Desde Formulario</h1>
+    );
+}
+
+export default Formulario;
